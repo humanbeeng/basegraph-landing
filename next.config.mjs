cd2c7169a807983d8578a474924a66d7b3b76f60
Nithin SJ <thenithinraj@gmail.com>
2025-10-20 16:51:40 +0530

    chore: update next config tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'dist', // Custom output directory
+  output: 'export',
   typescript: {
     ignoreBuildErrors: false,
   },
